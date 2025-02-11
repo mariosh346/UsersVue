9210143232acabe8780d6a83424280ec8e7ab83d
Marios Tsigkas <mariosh346@gmail.com>
2025-02-11 21:08:33 +0200

    Refactor UserCollections component to utilize getUserItems for fetching user-specific collections

--- src/components/organisms/UserCollections.vue
+++ src/components/organisms/UserCollections.vue
@@ -1,21 +1,21 @@
 <template>
   <main>
     <q-list v-if="userData">
-      <user-collection
+      {{ userData }}
+      <!-- <user-collection
         v-for="collection in userData.collections"
         :key="collection"
         :collection="collection"
-      />
+      /> -->
     </q-list>
     <NewUserCollection />
   </main>
 </template>
 <script setup lang="ts">
 import { useUserStore } from 'src/stores/userStore';
-import { getUserData } from 'src/utlils/firestore/composables';
-import UserCollection from 'src/components/organisms/UserCollection.vue';
+import { getUserItems, } from 'src/utlils/firestore/composables';
 import NewUserCollection from './NewUserCollection.vue';
 
 const userStore = useUserStore();
-const userData = getUserData(userStore?.uid);
+const userData = getUserItems(userStore.uid);
 </script>
--- src/utlils/firestore/composables.ts
+++ src/utlils/firestore/composables.ts
@@ -1,4 +1,4 @@
-import { collection, doc } from "firebase/firestore"
+import { collection, doc, query, where } from "firebase/firestore"
 import { useCollection, useDocument } from "vuefire"
 import { db } from "./db"
 import type { User } from "firebase/auth"
@@ -9,4 +9,8 @@ export const getUserData = (id: string) => useDocument<User>(getUserDoc(id))
 export const getItemDoc = (id: string) => doc(db, 'collections', id)
 export const getItem = (id: string) => useDocument(getItemDoc(id))
 
-export const getItems = () => useCollection(collection(db, 'collections'))
+export const getUserItems = (uid: User["uid"]) => {
+const colRef = collection(db, "collections");
+const filteredQuery = query(colRef, where("userId", "==", uid));
+return useCollection(filteredQuery);
+}
