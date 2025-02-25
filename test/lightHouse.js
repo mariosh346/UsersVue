59526513e82a09df8863b3313bfd795265214894
Marios.Tsigkas <m.tsigkas@expondo.com>
2025-02-25 14:57:33 +0200

    Add Lighthouse script to package.json and refactor error handling in lightHouse.js

--- test/lightHouse.js
+++ test/lightHouse.js
@@ -5,14 +5,19 @@ import * as chromeLauncher from 'chrome-launcher';
 const BASE_URL = 'http://localhost:9000';
 const previousScore = 40
 
+function terminateProcess(chrome, exitCode) {
+  chrome.kill();
+  process.exit(exitCode);
+}
+
 const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
 const options = {logLevel: 'info', output: 'html', port: chrome.port};
 const runnerResult = await lighthouse(BASE_URL, options);
 
 if (!runnerResult) {
   console.error('Lighthouse failed to generate a report');
-  chrome.kill();
-  process.exit(1);
+  terminateProcess(chrome, 1);
+  return
 }
 const reportHtml = Array.isArray(runnerResult.report) ? runnerResult.report.join('\n') : runnerResult.report;
 fs.writeFileSync('lhreport.html', reportHtml);
@@ -22,14 +27,14 @@ console.log('Report is done for', runnerResult.lhr.finalDisplayedUrl);
 const score = runnerResult?.lhr?.categories?.performance?.score;
 if (!score) {
   console.error('Lighthouse failed to generate a performance score');
-  chrome.kill();
-  process.exit(1);
+  terminateProcess(chrome, 1);
+  return
 }
 console.log('Performance score was', score * 100);
 if (score * 100 < previousScore) {
   console.error('Performance score is lower than previous score');
-  chrome.kill();
-  process.exit(1);
+  terminateProcess(chrome, 1);
+  return
 }
 
-chrome.kill();
+terminateProcess(chrome, 0);
