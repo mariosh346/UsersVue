42f9357430faea786ca2681f2645e99ac9d64ea0
Marios Tsigkas <mariosh346@gmail.com>
2025-03-18 15:14:06 +0200

    Refactor Firestore composables to use a dedicated useItemsCollection function for improved code clarity and reusability

--- src/utlils/firestore/composables.ts
+++ src/utlils/firestore/composables.ts
@@ -2,20 +2,26 @@ import { query, where } from "firebase/firestore";
 import { useCollection, useDocument } from "vuefire";
 import { getItemDoc, getUserDoc, itemsCollection } from "./db";
 import type { User } from "firebase/auth";
+import { UserCollectionDocument } from "src/types/forms";
 
 export const getUserData = (id: string) => useDocument<User>(getUserDoc(id));
 
 export const getItem = (id: string) => useDocument(getItemDoc(id));
 
+export const useItemsCollection = (collectionId: Parameters<typeof useCollection>[0]) => {
+
+  return useCollection<UserCollectionDocument>(collectionId);
+};
+
+
 export const getUserItems = (uid: User["uid"]) => {
 
   const filteredQuery = query(itemsCollection, where("userId", "==", uid));
-  return useCollection(filteredQuery);
+  return useItemsCollection(filteredQuery);
 
 };
 
 export const useAllItemsCollection = () => {
 
-  return useCollection(itemsCollection);
-
+  return useItemsCollection(itemsCollection);
 };
