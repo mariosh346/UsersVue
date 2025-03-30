af55c883329da25f1a4ba0b0e34fb98a9c4c0c60
Marios Tsigkas <mariosh346@gmail.com>
2025-03-30 16:24:37 +0300

    Add Vue I18n setup for localization support in Vitest configuration

--- test/vitest/setup-file.ts
+++ test/vitest/setup-file.ts
@@ -1,1 +1,12 @@
+import { createI18n } from 'vue-i18n'
+import { config } from '@vue/test-utils'
+
 // This file will be run before each test file
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+  messages: { en: {} }
+})
+
+config.global.plugins = [i18n]
