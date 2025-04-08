46ecc2e0ae33d2cf0c4212e34789bb0d0889437d
Marios Tsigkas <mariosh346@gmail.com>
2025-04-08 20:45:25 +0300

    Enhance Vite configuration to exclude specific files

--- quasar.config.ts
+++ quasar.config.ts
@@ -67,7 +67,16 @@ export default defineConfig((ctx) => {
       // polyfillModulePreload: true,
       // distDir
 
-      // extendViteConf (viteConf) {},
+      extendViteConf () {
+        return {
+          build: {
+            // Exclude files with the *.cy.tsx extension
+            rollupOptions: {
+              external: ['functions/**', '**.cy.ts', 'vitest.config.*'],
+            }
+          }
+        };
+      },
       // viteVuePluginOptions: {},
 
       vitePlugins: [
@@ -107,10 +116,10 @@ export default defineConfig((ctx) => {
                 if (!match.includes('lang=')) {
 
                   return match.replace('<script', '<script lang="ts"');
-                
+
                 }
                 return match;
-              
+
               });
 
               // Add lang="scss" to <style> blocks
@@ -119,15 +128,15 @@ export default defineConfig((ctx) => {
                 if (!match.includes('lang=')) {
 
                   return match.replace('<style', '<style scoped lang="scss"');
-                
+
                 }
                 return match;
-              
+
               });
-            
+
             }
             return code;
-          
+
           },
         },
       ]
